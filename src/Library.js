f5d9a2039f9eab54f1dde1811c9bb675a3ec1f9a
Marianne Barton <mhood24@gmail.com>
2022-05-15 15:30:53 -0600

    Unskip tests, write function

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,40 @@
 
+function createLibrary(libraryInfo) {
+  return {
+    name: libraryInfo,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction:[],
+    }
+
+    }
+
+  };
+
+  function addBook(libraryLocation, books) {
+    if (books.genre == "fantasy") {
+      libraryLocation.shelves.fantasy.unshift(books);
+    } else if
+      (books.genre == "fiction") {
+        libraryLocation.shelves.fiction.unshift(books);
+    } else {
+        libraryLocation.shelves.nonFiction.unshift(books);
+      }
+    }
+
+function checkoutBook(place, book, genre) {
+  for (var i = 0; i < place.shelves[genre].length; i++) {
+    if (book === place.shelves[genre][i].title) {
+      place.shelves[genre].splice(i, 1);
+    return `You have now checked out ${book} from the ${place.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${place.name}`;
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
